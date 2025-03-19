24fadf50ee5bd42feaaf947a027569340f76158a
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-19 19:34:08 +0530

    Finished lossless file compression

--- Firmware/IO/compression.cpp
+++ Firmware/IO/compression.cpp
@@ -1,8 +1,23 @@
 class compression{
     public:
-        char* lossless_compression ()
+        char* lossless_compression (uint8192_t window_size, char *filestream)
         {
             char *compressed_text;
+            uint8192_t previous_max = 0, current_max = filestream[0], temporary_max = 0;
+
+            if (window_size <= sizeof(filestream))
+            {
+                for (int i = 0; i < sizeof(filestream); i++)
+                {
+                    if (previous_max > current_max)
+                    {
+                        temporary_max = current_max;
+                        current_max = previous_max;
+                        previous_max = temporary_max;
+                    }
+                }
+            }
+
             return compressed_text;
         }
 
