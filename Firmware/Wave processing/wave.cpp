0dc51b96d7f63a29ab18fe39f666072e5d204027
Tarun Kumar <catchtherainbow6@protonmail.com>
2025-03-21 17:38:15 +0530

    Added comments to explain what is happening in th function

--- Firmware/Wave processing/wave.cpp
+++ Firmware/Wave processing/wave.cpp
@@ -8,32 +8,32 @@ class wave{
             *. @return generated wave in digital values.
             */
 
-            long maximum_resolution = 2;
+            long maximum_resolution = 2;// 2^0
            
             for (int i = 2; i <= bit_depth; i++)
-                maximum_resolution = maximum_resolution * maximum_resolution;
-            long wave_in_digital_levels [maximum_resolution * 4], temporary_number =  bit_depth;
-            wave_in_digital_levels [0] = 1;
+                maximum_resolution = maximum_resolution * maximum_resolution; //Exponentiating until the maximum resolution.
+            long wave_in_digital_levels [maximum_resolution * 4], temporary_number =  bit_depth; // 4 times for positive half (x2) (above 0) and negative half (x2) (below 0).
+            wave_in_digital_levels [0] = 1; //2^0.
 
-            for (int i = 1; i <= maximum_resolution; i++)
+            for (int i = 1; i <= maximum_resolution; i++) //Positive half
             {
                 if (i == 1)
                     wave_in_digital_levels [i] = bit_depth;
                 else if (i > 1)
                 {
-                    temporary_number = temporary_number * temporary_number;
+                    temporary_number = temporary_number * temporary_number; //Ascending
                     wave_in_digital_levels [i] = temporary_number;
                 }
             }
             
-            for (int i = maximum_resolution; i >= 0 ; i--)
+            for (int i = maximum_resolution; i >= 0 ; i--) //Positive half
             {
-                wave_in_digital_levels [i] = temporary_number/2;
+                wave_in_digital_levels [i] = temporary_number/2;//Descending
                 temporary_number = temporary_number/2;
             }
  
             for (int i = 0; i < maximum_resolution; i++)
-                wave_in_digital_levels [i+maximum_resolution] = -(wave_in_digital_levels[i]);
+                wave_in_digital_levels [i+maximum_resolution] = -(wave_in_digital_levels[i]); //Negative half
             return wave_in_digital_levels;
         }
 };
