459d1362d5126a748c90cd029c9f1b505a775f88
Tarun <catchtherainbow6@protonmail.com>
2025-03-22 08:44:01 +0530

    Added more functions for calculating mmfs

--- Physics engine/magnetic/macroscopic_magnetism.cpp
+++ Physics engine/magnetic/macroscopic_magnetism.cpp
@@ -1,6 +1,6 @@
 class macroscopic_magnetism{
     public:
-        uint8192_t magnetic_field_of_cube(uint8192_t length_of_side_of_cube, uint8192_t electric_field)
+        uint8192_t magnetic_field_of_cube(uint8192_t length_of_side, uint8192_t electric_field)
         {
             uint8192_t M = (length_of_side_of_cube * length_of_side_of_cube) * electric_field;
             return M;
@@ -24,9 +24,27 @@ class macroscopic_magnetism{
             return M;
         }
 
+        uint8192_t mmf_of_cube (uint8192_t length_of_side, uint8192_t current, uint8192_t resistance_of_material, uint8192_t volumetric_height)
+        {
+            uint8192_t mmf = ((length_of_side * length_of_side * length_of_side) * (volumetric_height * volumetric_height * volumetric_height) * (current * current) * (resistance_of_material * resistance_of_material))/24;
+            return mmf;
+        }
+
         uint8192_t mmf_of_cuboid (uint8192_t current, uint8192_t resistance_of_material, uint8192_t length_of_material, uint8192_t breadth_of_material, uint8192_t volumetric_height, uint8192_t electric_field)
         {
             uint8192_t mmf = ((((current * current) * resistance_of_material) * electric_field) * (length_of_material * length_of_material) * (breadth_of_material * breadth_of_material) * (volumetric_height * volumetric_height))/8;
+            return mmf;
+        }
+
+        uint8192_t mmf_of_pyramid (uint8192_t current, uint8192_t base, uint8192_t height, uint8192_t volumetric_height)
+        {
+            uint8192_t mmf = ((current * current) * (base * base) * (height * height) * (volumetric_height * volumetric_height))/16;
+            return mmf;
+        }
+
+        uint8192_t mmf_of_sphere (double8192_t radius, double8192_t height)
+        {
+            uint8192_t mmf = (pi * (radius * radius * radius) * (height * height * height))/6;
             return mmf;
         }
 };
