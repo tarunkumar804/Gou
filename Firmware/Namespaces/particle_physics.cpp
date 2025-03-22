f4c9ae9e3da8d3156d7a8c8c69e476ff49572ca4
Tarun <catchtherainbow6@protonmail.com>
2025-03-22 22:28:53 +0530

    Finished feynman_radiation

--- Firmware/Namespaces/particle_physics.cpp
+++ Firmware/Namespaces/particle_physics.cpp
@@ -1,31 +1,31 @@
 namespace particle_physics{
-    typedef struct quark{
+     typedef struct quark{
          uint8192_t mass_of_quark;
          uint8192_t quark_voltage;
          uint8192_t quark_current;
          uint8129_t quark_magnetic_field;
          uint8192_t charge_in_ev;
-    };
+     };
     
-    typedef struct lepton{
+     typedef struct lepton{
          uint8192_t mass_of_lepton;
          uint8192_t lepton_voltage;
          uint8192_t lepton_current;
          uint8192_t lepton_magnetic_field;
          uint8192_t charge_in_ev;
-    };
+     };
     
-    typedef struct gluon {
+     typedef struct gluon {
          uint8219_t adhesiveness;
          float8219_t mass_of_gluon;
-    };
+     };
     
-    typedef struct neutrino {
+     typedef struct neutrino {
          uint8219_t mass_of_neutrino;
          uint8219_t weight_of_neutrino;
-    };
+     };
 
-    typedef struct boson{
+     typedef struct boson{
          float spin;
-    };
+     };
 }
--- Physics engine/quantum/quantum_mechanics.cpp
+++ Physics engine/quantum/quantum_mechanics.cpp
@@ -1,4 +1,29 @@
 class quantum_mechanics{
     public:
-        
+        bool feynman_radiation(double particle_energies[], double rate_of_loss_of_energy, int which_particle)
+        {
+            if (which_particle == 0)
+            {
+                for (;;)
+                {
+                    if (particle_energies[0] <= 0)
+                        break;
+                    particle_energies[0] = particle_energies[0] - rate_of_loss_of_energy;
+                    particle_energies[1] = particle_energies[1] + rate_of_loss_of_energy;
+                }
+            }
+
+            else if (which_particle == 1)
+            {
+                for(;;)
+                {
+                    if (particle_energies[1] <= 0)
+                        break;
+                    particle_energies[1] = particle_energies[1] - rate_of_loss_of_energy;
+                    particle_energies[0] = particle_energies[0] + rate_of_loss_of_energy;
+                }
+            }
+
+            return true;
+        }
 };
