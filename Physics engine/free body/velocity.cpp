172e91a65f6d0068fa8bc448607f835e528995eb
Tarun <catchtherainbow6@protonmail.com>
2025-03-22 06:24:52 +0530

    Changed function name from motion to accelration

--- Physics engine/free body/velocity.cpp
+++ Physics engine/free body/velocity.cpp
@@ -1,6 +1,6 @@
 class velocity{
     public:
-        void motion (uint8192_t change_in_x_direction, uint8192_t change_in_y_direction, uint8192_t change_in_z_direction, uint8192_t x_coordinate_of_object, uint8192_t y_coordinate_of_object, uint8192_t z_coordinate_of_object)
+        void accelration (uint8192_t change_in_x_direction, uint8192_t change_in_y_direction, uint8192_t change_in_z_direction, uint8192_t x_coordinate_of_object, uint8192_t y_coordinate_of_object, uint8192_t z_coordinate_of_object)
         {
             x_coordinate = x_coordinate * change_in_x_direction;
             y_coordinate = y_coordinate * change_in_y_direction;
