28eebaac96ef02b9b3f2c9f3e9010bf37531d5a0
Tarun <catchtherainbow6@protonmail.com>
2025-03-22 06:41:37 +0530

    Finished deamplify_sound

--- Physics engine/sound/macroscopic_sound.cpp
+++ Physics engine/sound/macroscopic_sound.cpp
@@ -1,11 +1,18 @@
 class macroscopic_sound{
     public:
-        long* amplify_sound(long amplification_amount, long bit_depth, double starting_degree)
+        long* amplify_sound (long amplification_amount, long bit_depth, double starting_degree)
         {
-            long* sound_wave = wave.generate_wave(starting_degree, bit_depth);
+            long *sound_wave = wave.generate_wave(starting_degree, bit_depth);
             
             for (long l  = 0; l < sizeof(sound_wave); l++)
                 sound_wave[l] = sound_wave[l] + amplification_amount;
+            return sound_wave;
+        }
+
+        long* deamplify_sound (long deamplify_amount, long *sound_wave)
+        {
+            for (long l = 0; l < sizeof(sound_wave); l++)
+                sound_wave[l] = sound_wave[l] - deamplify_amount;
             return sound_wave;
         }
 };
