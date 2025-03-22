4e083088c406d712ce3c4f8d106963619191351a
Tarun <catchtherainbow6@protonmail.com>
2025-03-22 07:25:38 +0530

    Finished mmf_of_cuboid

--- Physics engine/magnetic/macroscopic_magnetism.cpp
+++ Physics engine/magnetic/macroscopic_magnetism.cpp
@@ -1,5 +1,11 @@
 class macroscopic_magnetism{
     public:
+        uint8192_t magnetic_field_of_cube(uint8192_t length_of_side_of_cube, uint8192_t electric_field)
+        {
+            uint8192_t M = (length_of_side_of_cube * length_of_side_of_cube) * electric_field;
+            return M;
+        }
+
         uint8192_t magnetic_field_of_cuboid (uint8192_t length_of_object, uint8192_t electric_field)
         {
             uint8192_t M = (electric_field * length_of_object * length_of_object)/2;
@@ -16,5 +22,12 @@ class macroscopic_magnetism{
         {
             uint8192_t M = (((base_of_triangle * height_of_triangle)/2) * volumetric_height) * electric_field;
             return M;
+        }
+
+        uint8192_t mmf_of_cuboid (uint8192_t current, uint8192_t resistance_of_material, uint8192_t length_of_material, uint8192_t electric_field)
+        {
+            uint8192_t mmf = ((((current * current) * resistance_of_material) * electric_field) * length_of_material * length_of_material)/2;
+            return mmf;
+
         }
 };
