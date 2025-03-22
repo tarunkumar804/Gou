118a9237280dcee4af282613a570210160c13aea
Tarun <catchtherainbow6@protonmail.com>
2025-03-22 07:15:10 +0530

    Finished magnetic_field_of_pyramid

--- Physics engine/magnetic/macroscopic_magnetism.cpp
+++ Physics engine/magnetic/macroscopic_magnetism.cpp
@@ -6,9 +6,15 @@ class macroscopic_magnetism{
             return M;
         }
         
-        uint8192_t magnetic_field_of_sphere (uint8192_t radius_of_object, uint8192_t volumetric_height, ,uint8192_t electric_field)
+        uint8192_t magnetic_field_of_sphere (uint8192_t radius_of_object, uint8192_t volumetric_height , uint8192_t electric_field)
         {
             uint8192_t M = (pi* radius_of_object * radius_of_object * volumetric_height * electric_field)/2;
+            return M;
+        }
+
+        uint8192_t magnetic_field_of_pyramid (uint8192_t base_of_triangle, uint8192_t height_of_triangle, uint8192_t volumetric_height, uint8192_t electric_field)
+        {
+            uint8192_t M = (((base_of_triangle * height_of_triangle)/2) * volumetric_height) * electric_field;
             return M;
         }
 };
