3170c8644d1b687744b6156297e767709a6bb6ff
Tarun <catchtherainbow6@protonmail.com>
2025-03-25 16:57:38 +0530

    Finished mean and variance of binomial distribution

--- Mathematics/distributions.cpp
+++ Mathematics/distributions.cpp
@@ -2,7 +2,6 @@ class binomial{
     public:
         double* pmfs (double set[], long number_of_trials)
         {
-
             double pmfs[sizeof(set)], P_X = 0, q = 0;
             long count = 0, n_factorial = 1, n_r_factorial = 1, ncr = 1, r_factorial = 1, trial = 0;
 
@@ -45,5 +44,42 @@ class binomial{
             }
 
             return pmfs;
+        }
+
+        double* means (double set[])
+        {
+            double mean_array[sizeof(set)], P_X = 0, q = 0;
+            long count = 0;
+
+            for (long l = 0; l < sizeof(set); l++)
+            {
+                for (long m = 0; m < sizeof(set); m++)
+                    if (set[l] == set[m])
+                        count ++;
+                
+                P_X = count/sizeof(set);
+
+                mean_array [l] = sizeof(set) * P_X;
+            }
+
+            return mean_array;
+        }
+
+        double* variances (double set[])
+        {
+            double variances[sizeof(set)], P_X = 0, q = 0;
+            long count = 0;
+
+            for (long l = 0; l < sizeof(set); l++)
+            {
+                for (long m = 0; m < sizeof(set); m++)
+                    if (set[l] == set[m])
+                        count ++;
+                P_X = count/sizeof(set);
+                q = 1 - P_X;
+                variances[l] = sizeof(set) * P_X * q;
+            }
+
+            return variances;
         }
 };
