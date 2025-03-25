66e44ac6cfd008dbe1365b22cb481a98c67c608a
Tarun <catchtherainbow6@protonmail.com>
2025-03-25 10:16:06 +0530

    Added a function to implement LDR

--- Compiler/x86.cpp
+++ Compiler/x86.cpp
@@ -1,4 +1,10 @@
 class x86{
     public:
-        
+        char* LDR (uint8192_t address_of_first_register, uint8192_t address_of_second_register)
+        {
+            char *operand1 = '0' + address_of_first_register;
+            char *operand2 = '0' + address_of_second_register;
+            char *instruction = {"LDR", operand1, operand2};
+            return instructon;
+        }
 };
