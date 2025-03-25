16519c5964514ba196cba5bab49d2c3a20cd3f00
Tarun <catchtherainbow6@protonmail.com>
2025-03-25 19:07:14 +0530

    Finished is_linear_combination

--- Mathematics/vector_calculus.cpp
+++ Mathematics/vector_calculus.cpp
@@ -1,5 +1,6 @@
 class vector_calculus{
     public:
+
         double** subspace_sums (double set[][]){
             double subspace_sums [sizeof(set[0])], sum = 0;
             
@@ -13,5 +14,28 @@ class vector_calculus{
             }
 
             return subspace_sums;
+        }
+
+        bool is_linear_combination(double vector1[], double vector2[], double vector3[], double multiplication_factor1, double multiplication_factor2)
+        {
+            double sum1 = 0, sum2 = 0, sum3 = 0;
+
+            for (long l = 0; l < sizeof(vector1); l++)
+            {
+                vector2[l] = vector2[l] * multiplication_factor1;
+                sum2 = sum2 + vector2[l];
+            }
+
+            for (long l = 0; l < sizeof(vector2); l++)
+            {
+                vector3[l] = vector3[l] * multiplication_factor2;
+                sum3 = sum3 + vector3[l];
+            }
+
+            for (long l = 0; l < sizeof(vector1); l++)
+                sum1 = sum1 + vector1[l];
+            if (sum1 == (sum2 + sum3))
+                return false;
+            return true;
         }
 };
