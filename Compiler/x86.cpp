6f998b7dde640e71cefb68b65ea0117bdd8d3cdd
Tarun <catchtherainbow6@protonmail.com>
2025-03-25 16:36:11 +0530

    Finished pmf of binomial distribution

--- Compiler/x86.cpp
+++ Compiler/x86.cpp
@@ -5,6 +5,14 @@ class x86{
             char *operand1 = '0' + address_of_first_register;
             char *operand2 = '0' + address_of_second_register;
             char *instruction = {"LDR", operand1, operand2};
-            return instructon;
+            return instruction;
+        }
+
+        char* ADD (uint8192_t address_of_first_register, uint8192_t address_of_second_register)
+        {
+            char *operand1 = '0' + address_of_first_register;
+            char *operand2 = '0' + address_of_second_register;
+            char *instruction = {"ADD", operand1, operand2};
+            return instruction;
         }
 };
