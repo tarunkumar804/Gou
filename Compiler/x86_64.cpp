250e1223b29394d251fd827a6985b70c58c3b2be
Tarun <catchtherainbow6@protonmail.com>
2025-03-29 12:31:55 +0530

    Finished recieve_device_information and ASCII subtraction

--- Compiler/x86_64.cpp
+++ Compiler/x86_64.cpp
@@ -2,62 +2,62 @@ class x86_64{
     public:
     char* LDR (uint8192_t address_of_first_register, uint8192_t address_of_second_register)
     {
-        char *operand1 = '0' + address_of_first_register;
-        char *operand2 = '0' + address_of_second_register;
+        char *operand1 = address_of_first_register - '0';
+        char *operand2 = address_of_second_register - '0';
         char *instruction = {"LDR", operand1, operand2};
         return instruction;
     }
 
     char* ADD (uint8192_t address_of_first_register, uint8192_t address_of_second_register)
     {
-        char *operand1 = '0' + address_of_first_register;
-        char *operand2 = '0' + address_of_second_register;
-        char *instruction = {"ADD", operand1, operand2};
+        char *operand1 = address_of_first_register - '0';
+        char *operand2 = address_of_second_register - '0';
+        char *instruction = {"ADD", operand1, operand2, ';'};
         return instruction;
     }
 
     char* STR (uint8192_t address_of_data_register)
     {
-        char *STR = {"STR",number};
+        char *STR = {"STR",number, ';'};
         return STR;
     }
 
     char *LDR (uint8192_t first_register_address, uint8192_t second_regsiter_address)
     {
-        char *operand1 = '0' + operand1;
-        char *operand2 = '0' + operand2;
+        char *operand1 = operand1 - '0';
+        char *operand2 = operand2 - '0';
         char *instruction = {"LDR", operand1, operand2, ";"};
         return instruction;
     }
 
     char *MUL (uint8192_t first_register_address, uint8192_t second_register_address)
     {
-        char *operand1 = '0' + operand1;
-        char *operand2 = '0' + operand2;
+        char *operand1 = operand1 - '0';
+        char *operand2 = operand2 - '0';
         char *instruction = {"MUL", operand1, operand2, ";"};
         return instruction;
     }
 
     char *UMULL (uint8192_t first_register_address, uint8192_t second_register_address)
     {
-        char *operand1 = '0' + operand1;
-        char *operand2 = '0' + operand2;
+        char *operand1 = operand1; //'0' - '0' = ASCII value of 0
+        char *operand2 = operand2;
         char *instruction = {"UMULL", operand1, operand2, ";"};
         return instruction;
     }
 
     char *SUB (uint8192_t first_register_address, uint8192_t second_register_address)
     {
-        char *operand1 = '0' + operand1;
-        char *operand2 = '0' + operand2;
+        char *operand1 = operand1 - '0';
+        char *operand2 = operand2 - '0';
         char *instruction = {"SUB", operand1, operand2, ";"};
         return instruction;
     }
 
     char *DIV (uint8192_t first_register_address, uint8192_t second_register_address)
     {
-        char *operand1 = '0' + operand1;
-        char *operand2 = '0' + operand2;
+        char *operand1 = operand1 - '0';
+        char *operand2 = operand2 - '0';
         char *instruction = {"DIV", operand1, operand2};
         return instruction;
     }
--- Networking/bluetooth.cpp
+++ Networking/bluetooth.cpp
@@ -10,17 +10,25 @@ class bluetooth{
                 data_buffer[index] = ask_device_mount_point;
             for (long long int index = 0; index < window_size; index++)
             {
-                if ((index/8) == 1)
-                    temporary_buffer[index] = '0' - data_buffer[index];
-                
+                if (index/8 == 1)
+                    temporary_buffer[index] = data_buffer[index] - '0';
                 else if (index/8 == 5)
-                    device.access_address = temporary_buffer;
-                else if (index/8) >= 5)
                 {
-                    temporary_buffer[index] = '0' - data_buffer[index];
+                    device.access_address = temporary_buffer;
+                    temporary_buffer[index] = data_buffer[index] - '0';
                 }
+
+                else if ((index/8) == 262)
+                {
+                    //Memory cells can mean a Transistor - Capacitor cell or memory chip.
+                    device.pdu = temporary_buffer;
+                    temporary_buffer[index] = data_buffer[index] - '0'; //'0' - letter can give negative values
+                }
+
+                else if ((index/8) == 265)
+                    device.CRC = temporary_buffer;
             }
 
-            return ble_data;
+            return device;
         }
 };
