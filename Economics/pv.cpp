535a53f1811b07c08e10a788870326887b77430c
Tarun <catchtherainbow6@protonmail.com>
2025-03-31 08:50:06 +0530

    Completed present value of an annuity

---
+++ Economics/pv.cpp
@@ -0,0 +1,32 @@
+class pv{
+    public:
+        
+        uint8192_t single_payment_present_amount (uint8192_t future_value, uint8192_t rate_of_interest, uint8192_t payment_periods)
+        {
+            double pv = 0;
+            
+            rate_of_interest = (1+rate_of_interest);
+            for (uint8192_t iteration = 0; iteration <= payment_periods; iteration++)
+                rate_of_interest = rate_of_interest * rate_of_interest;
+            pv = future_value/rate_of_interest;
+
+            return pv;
+        }
+
+        uint8192_t* periodic_payments_present_amount (uint8192_t future_amount, uint8192_t rate_of_interest, uint8192_t payment_periods)
+        {
+            uint8192_t temporary_copy = rate_of_interest;
+            rate_of_interest = (1 + rate_of_interest);
+
+            uint8192_t periodic_payments[periodic_payments];
+
+            for (uint8192_t payment = 0; payment < payment_periods; payment++)
+            {
+                for (uint8192_t compounding = 0; compounding < payment; compounding++)
+                    rate_of_interest = (rate_of_interest * rate_of_interest);
+                periodic_payments[payment] = (future_amount * temporary_copy)/(rate_of_interest - 1);
+            }
+
+            return periodic_payments;
+        }
+};
--- Graphics/shader.cpp
+++ Graphics/shader.cpp
@@ -1,11 +1,15 @@
 class shader{
     public:
         
-        uint8192_t shade_pixel(float percentage_shade, uint8192_t pixel)
+        uint8192_t pixel_shader (float percentage_shade, uint8192_t pixel)
         {
-            uint8192_t pixel = pixel - (percentage_shade*pixel);
+            pixel = pixel - (percentage_shade*pixel);
             return pixel;
         }
 
-        uint8192_t 
+        uint8192_t pixel_gradient (double gradienting_amount, uint8192_t pixel)
+        {
+            pixel = pixel/gradienting_amount;
+            return pixel;
+        }
 };
