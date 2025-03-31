88dc5374b1a41f031516690ad4d29e172cedd93f
Tarun <catchtherainbow6@protonmail.com>
2025-03-31 08:12:28 +0530

    Finished pixel_gradiennt

--- Graphics/shader.cpp
+++ Graphics/shader.cpp
@@ -1,8 +1,11 @@
 class shader{
     public:
-        uint8192_t shade_pixel(float percentage_shade, uiunt8192_t pixel)
+        
+        uint8192_t shade_pixel(float percentage_shade, uint8192_t pixel)
         {
             uint8192_t pixel = pixel - (percentage_shade*pixel);
             return pixel;
         }
+
+        uint8192_t 
 };
