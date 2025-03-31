f644ed9cf15f1b9c13a17d5e15a9b18bf888e887
Tarun <catchtherainbow6@protonmail.com>
2025-03-31 14:15:59 +0530

    Changed contents of accelration function

--- Graphics/renderer.cpp
+++ Graphics/renderer.cpp
@@ -10,11 +10,16 @@
 class Renderer {
     private:
     public:
-        bool draw_window(uint2048_t number_of_rows, uint2048_t number_of_columns, uint8192_t mount_point_of_GPU, uint8192_t display_port_address)
+
+        bool draw_window(uint2048_t number_of_rows, uint2048_t number_of_columns, uint8192_t monitor_mount_point)
         {
             /** A function to draw a window on the display.
-            * 
+            * @param number_of_rows - Number of rows in the image matrix.
+            * @param number_of_columns - Number of columns in the image matrix.
+            * @param monitor_mount_point - Mount point address of Monitor in RAM.
+            * @return Image to draw on screen.
             */
+
             bool window_drawn = false;
             uint3072_t  window[number_of_rows][number_of_columns];
             for (uint2048_t i = 0; i < number_of_rows; i++)
@@ -25,5 +30,10 @@ class Renderer {
                 for (uint8192_t j = 0; j < number_of_columns; j++)
                     send_to_display_port_address = window[i][j];
             return true;
+        }
+        
+        bool render_frames (frame frames[], long long int delay_in_rendering)
+        {
+            for 
         }
 };
--- Physics/free body/velocity.cpp
+++ Physics/free body/velocity.cpp
@@ -2,8 +2,8 @@ class velocity{
     public:
         void accelration (uint8192_t change_in_x_direction, uint8192_t change_in_y_direction, uint8192_t change_in_z_direction, uint8192_t x_coordinate_of_object, uint8192_t y_coordinate_of_object, uint8192_t z_coordinate_of_object)
         {
-            x_coordinate_of_object = x_coordinate_of_object * change_in_x_direction;
-            y_coordinate_of_object = y_coordinate_of_object * change_in_y_direction;
-            z_coordinate_of_object = z_coordinate_of_object * change_in_z_direction;
+            x_coordinate_of_object = x_coordinate_of_object + (x_coordinate_of_object * change_in_x_direction); // Acceleration of object in X - coordinate.
+            y_coordinate_of_object = y_coordinate_of_object + (y_coordinate_of_object * change_in_y_direction); // Acceleration of object in Y - coordinate.
+            z_coordinate_of_object = z_coordinate_of_object + (z_coordinate_of_object * change_in_z_direction); // Acceleration of object in Z - coordinate.
         }
 };
