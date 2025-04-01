55928249b569bb6d932a3dae464cef65e6c13811
Tarun <catchtherainbow6@protonmail.com>
2025-04-01 10:11:03 +0530

    Added authors of functions and changed many

--- Economics/fv.cpp
+++ Economics/fv.cpp
@@ -1,35 +1,36 @@
 class fv{
-    double single_payment(double principal, float rate_of_interest, long long int payment_periods)
-    {
+    double single_payment(double principal, float rate_of_interest, long long int payment_periods){
         /** A function to calculate future value of a single amount.
         * @param principal - Initial amount of the series.
         * @param rate_of_interest - Interest rate in percentage.
         * @param payment_periods - Number of payment periods.
         * @return Future value of a single payment.
+        * Implements the equation : F = P + P(1+r)^n
         */
 
-        double fv = 0;
-        rate_of_interest = 1 + rate_of_interest;
+        double fv = 0, rate_of_interest_copy = rate_of_interest;
+
+        rate_of_interest_copy = 1 + rate_of_interest;
         for (long long int payment = 0; payment < payment_periods; payment++)
-            rate_of_interest = rate_of_interest * rate_of_interest;
-        fv = principal * rate_of_interest;
+            rate_of_interest_copy = rate_of_interest_copy * rate_of_interest_copy;
+        fv = principal + (principal * rate_of_interest);
         return fv;
     }
 
-    double annual_payments (double *annuities, float rate_of_interest, long long int payment_periods)
-    {
+    double annual_payments (double *annuities, float rate_of_interest, long long int payment_periods){
         /** A function to calculate future value of annuities.
         * @param annuities - An array of periodic payments.
         * @param rate_of_interest - Rate of interest in percentages.
         * @param payment_periods - Number of payment periods.
-        * @return future value.
+        * @return annual payments.
+        * Implements the equation : F = A *((1+i)^n - 1/i)
         */
 
-        double fv = 0;
+        double fv  = 0;
         float interest_copy = 1 + rate_of_interest;
 
         for (long long int payment = 0; payment < payment_periods; payment++)
-            fv = fv + annuities[payment] * (((interest_copy * interest_copy) - 1)/rate_of_interest);
+            fv = fv + annuities[payment] * (((interest_copy * interest_copy) - 1)/rate_of_interest); //Adds all the annuities after calculating the compounding factor.
         return fv;
     }
 };
--- Economics/pv.cpp
+++ Economics/pv.cpp
@@ -1,37 +1,49 @@
 class pv{
     public:
         
-        uint8192_t single_payment_present_amount (uint8192_t future_value, uint8192_t rate_of_interest, uint8192_t payment_periods)
+        double single_payment_present_amount (double future_value, double rate_of_interest, long long int payment_periods)
         {
+            /** Implements single present value of a future amount.
+            * Equation : P = F/((1+r)^n).
+            * @author Tarun Kumar, Leornardo Di Maximillian.
+            * @param future_value - Future value of a single payment.
+            * @param rate_of_interest - Interest rate of the period.
+            * @param payment_periods - Number of periods.
+            * @return Returns the present value of a future amount.
+            */
+
             double pv = 0;
             
-            rate_of_interest = (1+rate_of_interest);
-            for (uint8192_t iteration = 0; iteration <= payment_periods; iteration++)
+            double rate_of_interest_copy = (1+rate_of_interest_copy);
+            for (long long int payment = 0; payment <= payment_periods; payment++)
                 rate_of_interest = rate_of_interest * rate_of_interest;
             pv = future_value/rate_of_interest;
 
             return pv;
         }
 
-        uint8192_t* periodic_payments_present_amount (uint8192_t future_amount, uint8192_t rate_of_interest, uint8192_t payment_periods)
+        double periodic_payments_present_amount (double future_amount, double annuities[] , double rate_of_interest, long long int payment_periods)
         {
-            uint8192_t temporary_copy = rate_of_interest;
-            rate_of_interest = (1 + rate_of_interest);
+            /** A function to implement present value of future sum of annuities.
+            * Equation : P = F/(Sum(A * ((1+i)^n - 1)/i)).
+            * @author Tarun Kumar , Leonardo Di Maximillian.
+            * @param future_amount - Future amount of a number of periodic payments.
+            * @param rate_of_interest - Rate of interest of the payment period.
+            * @param payment_periods - Number of periodic payments.
+            * @return present value of a number of periodic payments and future value.
+            */
 
-            uint8192_t periodic_payments[periodic_payments];
+            double pv = 0, annuity_sum = 0, interest_rate_copy = rate_of_interest;
+            interest_rate_copy =  1 + interest_rate_copy;
 
-            for (uint8192_t payment = 0; payment < payment_periods; payment++)
-            {
-                for (uint8192_t compounding = 0; compounding < payment; compounding++)
-                    rate_of_interest = (rate_of_interest * rate_of_interest);
-                periodic_payments[payment] = (future_amount * temporary_copy)/(rate_of_interest - 1);
-            }
-
-            return periodic_payments;
+            for (long long int payment; payment < payment_periods; payment++)
+                annuity_sum = annuity_sum + annuities[payment] *(((interest_rate_copy * interest_rate_copy) - 1)/interest_rate_copy);
+            pv = future_amount/annuity_sum;
+            return pv;
         }
         
-        uint8192_t present_value_of_arithmetic_gradient_series()
+        double present_value_of_arithmetic_gradient_series()
         {
-            
+               
         }
 };
